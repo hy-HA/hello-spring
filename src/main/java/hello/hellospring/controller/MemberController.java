710f2e71b13dfd9bc41ace4165721bc410576024
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-03-19 17:29:13 +0900

    mvc_home/register/view memberList

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -14,4 +20,31 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+     @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+     }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        //member.getName()확인
+        //System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        //memberService.findMembers();에서 옵션+cmd+v
+        List<Member> members = memberService.findMembers();
+        // "members"만 치면 attributeName 자동 생성됨.
+        // 멤버 리스트 자체를 모델에 담아서 뷰 템플에 넘길 것.
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
